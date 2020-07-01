0a7577a4fc6b88470ccdcafae2767dbb8e070c34
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-02 01:56:08 +0530

    added signup route

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,8 +1,48 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
 import { Link } from "react-router-dom";
+import { signup } from "../auth/helper";
 
 const Signup = () => {
+    const [values, setValues] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: false,
+    });
+
+    const { name, email, password, error, success } = values;
+
+    const handleChange = (name) => (event) => {
+        setValues({ ...values, error: false, [name]: event.target.value });
+    };
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setValues({ ...values, error: false });
+        signup({ name, email, password })
+            .then((data) => {
+                if (data?.error) {
+                    setValues({
+                        ...values,
+                        error: data?.error,
+                        success: false,
+                    });
+                } else {
+                    setValues({
+                        ...values,
+                        name: "",
+                        email: "",
+                        password: "",
+                        error: "",
+                        success: true,
+                    });
+                }
+            })
+            .catch(console.log("Error in signup"));
+    };
+
     const signUpForm = () => {
         return (
             <div className="row">
@@ -10,17 +50,32 @@ const Signup = () => {
                     <form>
                         <div className="form-group">
                             <label className="text-light">Name</label>
-                            <input className="form-control" type="text" />
+                            <input
+                                className="form-control"
+                                onChange={handleChange("name")}
+                                type="text"
+                            />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Email</label>
-                            <input className="form-control" type="email" />
+                            <input
+                                className="form-control"
+                                onChange={handleChange("email")}
+                                type="email"
+                            />
                         </div>
                         <div className="form-group">
                             <label className="text-light">Password</label>
-                            <input className="form-control" type="password" />
+                            <input
+                                className="form-control"
+                                onChange={handleChange("password")}
+                                type="password"
+                            />
                         </div>
-                        <button className="btn btn-success btn-block">
+                        <button
+                            onClick={onSubmit}
+                            className="btn btn-success btn-block"
+                        >
                             Submit
                         </button>
                     </form>
@@ -32,6 +87,7 @@ const Signup = () => {
     return (
         <Base title="Sign up page" description="A page for user to signup!">
             {signUpForm()}
+            <p className="text-white text-center">{JSON.stringify(values)}</p>
         </Base>
     );
 };
