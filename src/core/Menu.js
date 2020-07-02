6b8a590dac03aa29bb21e0742780a2a277b9700f
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-03 00:19:40 +0530

    done conditional renderig for admin and user dashboard

--- src/core/Menu.js
+++ src/core/Menu.js
@@ -33,25 +33,29 @@ const Menu = ({ history }) => (
                 </Link>
             </li>
 
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/user/dashboard")}
-                    className="nav-link"
-                    to="/user/dashboard"
-                >
-                    U. Dashboard
-                </Link>
-            </li>
+            {isAutheticated() && isAutheticated().user.role === 0 && (
+                <li className="nav-item">
+                    <Link
+                        style={currentTab(history, "/user/dashboard")}
+                        className="nav-link"
+                        to="/user/dashboard"
+                    >
+                        U. Dashboard
+                    </Link>
+                </li>
+            )}
+            {isAutheticated() && isAutheticated().user.role === 1 && (
+                <li className="nav-item">
+                    <Link
+                        style={currentTab(history, "/admin/dashboard")}
+                        className="nav-link"
+                        to="/admin/dashboard"
+                    >
+                        A. Dashboard
+                    </Link>
+                </li>
+            )}
 
-            <li className="nav-item">
-                <Link
-                    style={currentTab(history, "/admin/dashboard")}
-                    className="nav-link"
-                    to="/admin/dashboard"
-                >
-                    A. Dashboard
-                </Link>
-            </li>
             {!isAutheticated() && (
                 <Fragment>
                     <li className="nav-item">
