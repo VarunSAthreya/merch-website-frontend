5857220fcb5d5f328ae454066d5ad432b37389d7
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-05 01:11:28 +0530

    added stripe payment

--- src/paymentgateway/StripeCheckout.js
+++ src/paymentgateway/StripeCheckout.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { isAutheticated } from "../auth/helper";
 import { cartEmpty, loadCart } from "../core/helper/CartHelper";
 import { Link } from "react-router-dom";
+import StripeGateway from "react-stripe-checkout";
+import { API } from "../backend";
+import { createOrder } from "../core/helper/orderhelper";
 
 const StripeCheckout = ({
     products,
@@ -28,9 +31,42 @@ const StripeCheckout = ({
         return amount;
     };
 
+    const makePayment = (token) => {
+        const body = {
+            token,
+            products,
+        };
+        const headers = {
+            "Content-Type": "application/json",
+        };
+        return fetch(`${API}/stripepayment`, {
+            method: "POST",
+            headers,
+            body: JSON.stringify(body),
+        })
+            .then((response) => {
+                console.log(response);
+                const { status } = response;
+                console.log("STATUS", status);
+            })
+            .catch((err) => console.log(err));
+    };
+
     const showStripeButton = () => {
         return isAutheticated() ? (
-            <button className="btn btn-success rounded">Pay with Stripe</button>
+            <StripeGateway
+                stripeKey="pk_test_51H1Cw9EUqn7XzXigmB7RKLTzZHEtdR8Kpv3sLlDQOAwckuVaWWzHbD23KJBxQtgm7i9mh1AwWW485a4MpcE4L30V00tDFpi8qK"
+                token={makePayment}
+                amount={getFinalPrice() * 100}
+                name="Buy T-shirts"
+                currency="USD"
+                billingAddress
+                shippingAddress
+            >
+                <button className="btn btn-success rounded">
+                    Pay with Stripe
+                </button>
+            </StripeGateway>
         ) : (
             <Link to="/signin">
                 <button className="btn btn-warning rounded">Signin</button>
