26e5480119d10bcebf8b85cc442c752d58d8331b
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-04 16:27:32 +0530

    made products to show on home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
 import Base from "./Base";
 import Card from "./Card";
+import { getProducts } from "./helper/coreapicalls";
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState(false);
+
+    const loadAlProducts = () => {
+        getProducts().then((data) => {
+            if (data.error) {
+                setError(data.error);
+            } else {
+                setError(false);
+                setProducts(data);
+            }
+        });
+    };
+
+    useEffect(() => {
+        loadAlProducts();
+    }, []);
+
     return (
         <Base title="Home Page" description="Welcome to the T-shirt Store">
             <div className="row text-center">
-                <div className="col-4">
-                    <Card />
-                </div>
-                <div className="col-4">
-                    <button className="btn btn-success">TEST</button>
-                </div>
-                <div className="col-4">
-                    <button className="btn btn-success">TEST</button>
+                <h1 className="text-white">ALL of T-shirts</h1>
+                <div className="row">
+                    {products.map((product, index) => {
+                        return (
+                            <div key={index} className="col-4 mb-4">
+                                <Card product={product} />
+                            </div>
+                        );
+                    })}
                 </div>
             </div>
         </Base>
