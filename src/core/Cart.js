45f9f7542970fb9d8d406345938a3fe16a80f660
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-04 18:31:04 +0530

    added remove product from cart function

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -7,9 +7,11 @@ import { loadCart } from "./helper/CartHelper";
 const Cart = () => {
     const [products, setProducts] = useState([]);
 
+    const [reload, setReload] = useState(false);
+
     useEffect(() => {
         setProducts(loadCart());
-    }, []);
+    }, [reload]);
 
     const loadAllProducts = () => {
         return (
@@ -22,6 +24,8 @@ const Cart = () => {
                             product={product}
                             addtoCart={false}
                             removeFromCart={true}
+                            setReload={setReload}
+                            reload={reload}
                         />
                     );
                 })}
