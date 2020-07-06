5fd5a21a47e5cf6d8228d899a79e9f950b6a5cd4
Varun S Athreya <varun.sathreya@gmail.com>
2020-07-07 04:54:42 +0530

    added helper methodes for payment gateway

--- src/core/Cart.js
+++ src/core/Cart.js
@@ -6,46 +6,52 @@ import Card from "./Card";
 import { loadCart } from "./helper/cartHelper";
 
 const Cart = () => {
-  const [products, setProducts] = useState([]);
-  const [reload, setReload] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [reload, setReload] = useState(false);
 
-  useEffect(() => {
-    setProducts(loadCart());
-  }, [reload]);
+    useEffect(() => {
+        setProducts(loadCart());
+    }, [reload]);
 
-  const loadAllProducts = () => {
+    const loadAllProducts = (products) => {
+        return (
+            <div>
+                <h2>This section is to load products</h2>
+                {products.map((product, index) => (
+                    <Card
+                        key={index}
+                        product={product}
+                        removeFromCart={true}
+                        addtoCart={false}
+                        setReload={setReload}
+                        reload={reload}
+                    />
+                ))}
+            </div>
+        );
+    };
+    const loadCheckout = () => {
+        return (
+            <div>
+                <h2>This section for checkout</h2>
+            </div>
+        );
+    };
+
     return (
-      <div>
-        <h2>This section is to load products</h2>
-        {products.map((product, index) => (
-          <Card
-            key={index}
-            product={product}
-            removeFromCart={true}
-            addtoCart={false}
-            setReload={setReload}
-            reload={reload}
-          />
-        ))}
-      </div>
+        <Base title="Cart Page" description="Ready to checkout">
+            <div className="row text-center">
+                <div className="col-6">
+                    {products.length > 0 ? (
+                        loadAllProducts()
+                    ) : (
+                        <h3>No products in the cart</h3>
+                    )}
+                </div>
+                <div className="col-6">Payment Section</div>
+            </div>
+        </Base>
     );
-  };
-  const loadCheckout = () => {
-    return (
-      <div>
-        <h2>This section for checkout</h2>
-      </div>
-    );
-  };
-
-  return (
-    <Base title="Cart Page" description="Ready to checkout">
-      <div className="row text-center">
-        <div className="col-6">{loadAllProducts()}</div>
-        <div className="col-6">Payment Section</div>
-      </div>
-    </Base>
-  );
 };
 
 export default Cart;
